5e48325c352634e0edf6a2e735366959f0282e70
Inam Ul Haq <159928985+cyber-fanatic@users.noreply.github.com>
2024-05-18 23:22:46 +0500

    Update mac.zsh

--- mac.zsh
+++ mac.zsh
@@ -1,8 +1,10 @@
 #!/usr/bin/zsh
- 
+
+#script that changes your system MAC address : 
+
 sudo ifconfig wlan0 down
 
-sudo ifconfig wlan0 hw ether   2a:AB:2F:C3:Df:Cd
+sudo ifconfig wlan0 hw ether   2a:AB:2F:C3:Df:Cd 
 
 sudo ifconfig wlan0 up
 
